f2df43fe2dd95ebd90e57210c7d192c2b9b0e614
spaceguard <miha.masel1@gmail.com>
2024-10-02 18:01:57 +0300

    Lab 7

---
+++ src/lab7/Main.java
@@ -0,0 +1,48 @@
+package lab7;
+
+import java.util.Scanner;
+
+class Grade{
+    int grade;
+
+    public Grade(int grade){
+        this.grade = grade;
+    }
+
+    public int getter_grade(){
+        return grade;
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int counts;
+        while (true) {
+            try {
+                System.out.println("Введите количество баллов");
+                counts = input.nextInt();
+                if (counts > 100) {
+                    System.out.println("Максимальная оценка за экзамен 100 баллов!");
+                } else break;
+            } catch (Exception e) {
+                System.out.println("Вы ввели не число");
+            }
+        }
+
+
+        Grade grade = new Grade(counts);
+
+        if (grade.getter_grade() >= 85 && grade.getter_grade() <= 100) {
+            System.out.println("Оценка за экзамен: Отлично");
+        } else if (grade.getter_grade() >= 70 && grade.getter_grade() <= 84) {
+            System.out.println("Оценка за экзамен: Хорошо");
+        } else if (grade.getter_grade() >= 55 && grade.getter_grade() <= 69) {
+            System.out.println("Оценка за экзамен: Удовлетворительно");
+        } else {
+            System.out.println("Оценка за экзамен: Неудовлетворительно");
+        }
+
+    }
+}
